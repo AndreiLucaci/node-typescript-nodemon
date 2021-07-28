e19b50e170c798992d001afbd8f68ba5de7d3f4b
Andrei Lucaci <lucaciandrei@outlook.com>
2021-07-28 12:48:55 +0300

    chore: added skeleton for the project

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: ['./tsconfig.json'],
+    tsconfigRootDir: __dirname
+  },
+  plugins: ['prettier', '@typescript-eslint', 'unused-imports'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'prettier',
+    'prettier/@typescript-eslint'
+  ],
+  rules: {
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    'no-console': 'error',
+    '@typescript-eslint/no-empty-function': 'off',
+    'class-methods-use-this': 'off',
+    '@typescript-eslint/array-type': ['error', { default: 'array' }],
+    '@typescript-eslint/require-await': 'off',
+    'no-console': 'off',
+    'no-return-await': 'error',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'unused-imports/no-unused-imports': 'error'
+  },
+  env: {
+    jest: true,
+    node: true
+  }
+};
